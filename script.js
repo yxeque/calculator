5129a326e8afdf6730637899845f577007278032
yxeque <yxequel@gmail.com>
2024-01-17 16:35:38 +0800

    Add reference variables

---
+++ script.js
@@ -0,0 +1,8 @@
+// Reference
+const previousDisplay = document.querySelector("[data-previous-operand]");
+const currentDisplay = document.querySelector("[data-current-operand]");
+const numberBtn = document.querySelectorAll("[data-number]");
+const operatorBtn = document.querySelectorAll("[data-operator]");
+const equalsBtn = document.querySelector("[data-equals]");
+const deleteBtn = document.querySelector("[data-delete]");
+const clearBtn = document.querySelector("[data-clear]");
