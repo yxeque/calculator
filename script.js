b3077d4bd6695bc679aa7d3f3a2b6236b5ec0361
yxeque <yxequel@gmail.com>
2024-01-17 16:52:42 +0800

    Update Calculator class  - Check WebDevSimplified's Calculator tutorial for explanation, I'm just copy-pasting for now

--- script.js
+++ script.js
@@ -2,18 +2,88 @@ class Calculator {
   constructor(previousDisplay, currentDisplay) {
     this.previousDisplay = previousDisplay;
     this.currentDisplay = currentDisplay;
+    this.clear();
   }
-  clear() {}
+  clear() {
+    this.previousOperand = "";
+    this.currentOperand = "";
+    this.operation = undefined;
+  }
 
-  delete() {}
+  delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
+  }
 
-  appendNumber() {}
+  appendNumber(number) {
+    if (number === "." && this.currentOperand.includes(".")) return;
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+  }
 
-  compute() {}
+  chooseOperation(operation) {
+    if (this.currentOperand === "") return;
+    if (this.previousOperand !== "") {
+      this.compute();
+    }
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = "";
+  }
 
-  chooseOperation() {}
+  compute() {
+    let computation;
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (this.operation) {
+      case "+":
+        computation = prev + current;
+        break;
+      case "-":
+        computation = prev - current;
+        break;
+      case "*":
+        computation = prev * current;
+        break;
+      case "÷":
+        computation = prev / current;
+        break;
+      default:
+        return;
+    }
+    this.currentOperand = computation;
+    this.operation = undefined;
+    this.previousOperand = "";
+  }
 
-  updateDisplay() {}
+  getDisplayNumber(number) {
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split(".")[0]);
+    const decimalDigits = stringNumber.split(".")[1];
+    let integerDisplay;
+    if (isNaN(integerDigits)) {
+      integerDisplay = "";
+    } else {
+      integerDisplay = integerDigits.toLocaleString("en", {
+        maximumFractionDigits: 0,
+      });
+    }
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    } else {
+      return integerDisplay;
+    }
+  }
+
+  updateDisplay() {
+    this.currentDisplay.innerText = this.getDisplayNumber(this.currentOperand);
+    if (this.operation != null) {
+      this.previousDisplay.innerText = `${this.getDisplayNumber(
+        this.previousOperand
+      )} ${this.operation}`;
+    } else {
+      this.previousDisplay.innerText = "";
+    }
+  }
 }
 
 // Reference
@@ -24,3 +94,5 @@ const operatorBtn = document.querySelectorAll("[data-operator]");
 const equalsBtn = document.querySelector("[data-equals]");
 const deleteBtn = document.querySelector("[data-delete]");
 const clearBtn = document.querySelector("[data-clear]");
+
+const calculator = new Calculator(previousDisplay, currentDisplay);
